a046b49e17a1767e2a12733b42258b0ca611a41c
paulozemr <pauloramosj213@gmail.com>
2024-02-22 02:54:40 -0300

    Commit unico

---
+++ EscapeSequences/src/EscapeSequences.java
@@ -0,0 +1,11 @@
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+		System.out.println("como assim eu estou \"fedido\"?");
+		System.out.println("o seu arquivo está em ~\\desktop\\GatosFedidos");
+		System.out.println("olha so eu \t apertei tab");
+		System.out.println("olha \neu \napertando\nespaço");
+	}
+
+}
